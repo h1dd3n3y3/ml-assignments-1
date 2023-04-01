b46521a383cd293e606981fe03b3703034bd6ac1
david011011 <parathyrasdavid@gmail.com>
2023-04-01 12:05:46 +0300

    minorities

--- ERG1/erg1.py
+++ ERG1/erg1.py
@@ -1,7 +1,7 @@
 import requests, csv
 from datetime import datetime
 
-api_key = 'AFG1UCT1UGMCYXJ8'
+API_KEY = 'AFG1UCT1UGMCYXJ8'
 
 time_str_lower = '01/03/2022'
 time_obj_lower = datetime.strptime(time_str_lower, '%d/%m/%Y')
@@ -13,11 +13,11 @@ time_str_formatted_upper = time_obj_upper.strftime('%Y-%m-%d')
 
 STOCK_NAME = 'AAPL' # Apple stock name
 
-history_url = f'https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY_ADJUSTED&symbol={STOCK_NAME}&apikey={api_key}&datatype=csv'
+history_url = f'https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY_ADJUSTED&symbol={STOCK_NAME}&apikey={API_KEY}&datatype=csv'
 
 response = requests.get(history_url) # API call
 
-if response.status_code == 200: # Request code check
+if response.status_code == 200: # Call successful
     content = response.content.decode('utf-8') # Response content decoding
     
     reader = csv.DictReader(content.splitlines()) # Read the csv
@@ -27,7 +27,6 @@ if response.status_code == 200: # Request code check
     filtered_data = [row for row in data if time_str_formatted_lower <= row['timestamp'] <= time_str_formatted_upper]
     
     print(filtered_data)
-    print("\nFiltered {} total {} stock data between {} - {}.".format(len(filtered_data), STOCK_NAME, time_str_lower, time_str_upper))
-else:
-    # Print an error message if the request was not successful
+    print("\nFiltered {len(filtered_data)} total {STOCK_NAME} stock data between {time_str_lower} - {time_str_upper}.")
+else: # Call unsuccessful
     print(f'Request failed with status code {response.status_code}')
