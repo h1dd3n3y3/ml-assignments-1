aeb83d60f9f83b364f53c70d042a8d42cedebee6
david011011 <parathyrasdavid@gmail.com>
2023-04-01 12:09:30 +0300

    minorities

--- ERG1/erg1.py
+++ ERG1/erg1.py
@@ -3,13 +3,13 @@ from datetime import datetime
 
 API_KEY = 'AFG1UCT1UGMCYXJ8'
 
-time_str_lower = '01/03/2022'
+time_str_lower = '01/03/2022' # Desired starting date
 time_obj_lower = datetime.strptime(time_str_lower, '%d/%m/%Y')
-time_str_formatted_lower = time_obj_lower.strftime('%Y-%m-%d')
+time_str_formatted_lower = time_obj_lower.strftime('%Y-%m-%d') # Alpha Vantage timestamp pattern standard conversion
 
-time_str_upper = '01/10/2022'
+time_str_upper = '01/10/2022' # Desired ending date
 time_obj_upper = datetime.strptime(time_str_upper, '%d/%m/%Y')
-time_str_formatted_upper = time_obj_upper.strftime('%Y-%m-%d')
+time_str_formatted_upper = time_obj_upper.strftime('%Y-%m-%d') # Alpha Vantage timestamp pattern standard conversion
 
 STOCK_NAME = 'AAPL' # Apple stock name
 
@@ -22,9 +22,7 @@ if response.status_code == 200: # Call successful
     
     reader = csv.DictReader(content.splitlines()) # Read the csv
     data = [row for row in reader] # Save it in a list
-    
-    # Filter the data to only include the date/time range
-    filtered_data = [row for row in data if time_str_formatted_lower <= row['timestamp'] <= time_str_formatted_upper]
+    filtered_data = [row for row in data if time_str_formatted_lower <= row['timestamp'] <= time_str_formatted_upper] # Date filtration
     
     print(filtered_data)
     print("\nFiltered {len(filtered_data)} total {STOCK_NAME} stock data between {time_str_lower} - {time_str_upper}.")
