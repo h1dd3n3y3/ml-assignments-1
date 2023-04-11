771493e774d7174994af1f2d0b1a67dc04f01dc4
david011011 <parathyrasdavid@gmail.com>
2023-04-11 13:14:57 +0300

    edited comments

--- ERG1/erg1.py
+++ ERG1/erg1.py
@@ -5,11 +5,11 @@ API_KEY = 'AFG1UCT1UGMCYXJ8'
 
 time_str_lower = '01/03/2022' # Desired starting date
 time_obj_lower = datetime.strptime(time_str_lower, '%d/%m/%Y') # Assign time to datetime object
-time_str_formatted_lower = time_obj_lower.strftime('%Y-%m-%d') # Alpha Vantage timestamp pattern standard conversion
+time_str_formatted_lower = time_obj_lower.strftime('%Y-%m-%d') # Alpha Vantage timestamp pattern conversion
 
 time_str_upper = '01/10/2022' # Desired ending date
 time_obj_upper = datetime.strptime(time_str_upper, '%d/%m/%Y') # Assign time to datetime object 
-time_str_formatted_upper = time_obj_upper.strftime('%Y-%m-%d') # Alpha Vantage timestamp pattern standard conversion
+time_str_formatted_upper = time_obj_upper.strftime('%Y-%m-%d') # Alpha Vantage timestamp pattern conversion
 
 STOCK_NAME = 'AAPL' # Apple stock name
 
