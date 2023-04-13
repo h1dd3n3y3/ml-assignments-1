125166e463030e8550ea6c9a8363cf9272551435
david011011 <parathyrasdavid@gmail.com>
2023-04-14 02:32:48 +0300

    add diagram visualization

--- ERG1/erg1.py
+++ ERG1/erg1.py
@@ -33,5 +33,26 @@ if response.status_code == 200: # Call successful
     
     print(filtered_data, end="\n\n")
     print(f"Filtered {len(filtered_data)}/{len(data)} total '{STOCK_NAME}' stock data between {time_str_lower} - {time_str_upper}.")
+
+    # Define x and y axis references
+    x = [data['timestamp'] for data in filtered_data]
+    y_high = [float(data['high']) for data in filtered_data]
+    y_low = [float(data['low']) for data in filtered_data]
+
+    # Create plot
+    fig, ax = plt.subplots()
+    ax.plot(x, y_high, label='High')
+    ax.plot(x, y_low, label='Low')
+
+    # Set labels and title
+    ax.set_xlabel('Time')
+    ax.set_ylabel('Stock Value')
+    ax.set_title('Stock High and Low Values over Time')
+
+    plt.xticks(rotation=20) # Rotate x-axis labels for better visibility
+    fig.subplots_adjust(bottom=0.19) # Adjust spacing between subplots to make horizontal axis label visible
+
+    ax.legend() # Element to define line colours and labels
+    plt.show() # Display the plot
 else: # Call unsuccessful
     print(f'Request failed with status code {response.status_code}')
