2a183d475adac2f7b22b711ff234ca32ac09a0a9
wallmenis <sandboxgamergr@hotmail.com>
2023-04-24 19:25:38 +0300

    Added dataset DATASET_LICENSE and processed dataset. Also the base code for erg2 is implemented

---
+++ ERG2/erg2.py
@@ -0,0 +1,47 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import csv
+#from enum import Enum
+
+def safe_csv_read(csvstring):            #from python docs: https://docs.python.org/3/library/csv.html#examples
+    output=[]
+    with open(csvstring, newline='') as f:
+        reader = csv.reader(f)
+        for row in reader:
+            output.append(row)
+    return output
+
+#class Column(Enum):
+#    ArtGaleries = 0 
+#    DanceClubs = 1
+#    JuiceBars = 2
+#    Restaurants = 3
+#    Museums = 4
+#    Resorts = 5
+#    Parks = 6
+#    Beaches = 7
+#    Theaters = 8 
+#    ReligiousInstitutions = 9
+
+def manhatan_distance(point1_x,point1_y,point2_x,point2_y):   # eucledian distance is harder to compute
+    return np.abs(point1_x-point2_x) + np.abs(point1_y-point2_y)
+    
+# def makelabels(pairs):
+    
+
+data=safe_csv_read('source.csv')
+data.pop(0)
+#for i in data:  # After examining the data set, the user id is linearly assigned. So we don't need it as it can be recreated by adding 1 to the current index
+#    i.pop(0)
+                # At this point, the dataset is just a grid of numbers with each row being a user.
+
+#print(data[0][0])
+x=[]
+y=[]
+
+for i in data:
+    x.append(i[4])
+    y.append(i[12])
+plt.scatter(x,y)
+# plt.scatter(y)
+plt.show()
