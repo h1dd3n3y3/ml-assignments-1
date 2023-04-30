6ad2114c10498f637f1df847999ad5684e4a820a
wallmenis <sandboxgamergr@hotmail.com>
2023-04-30 13:09:19 +0300

    Added docs

--- ERG2/erg2.py
+++ ERG2/erg2.py
@@ -3,13 +3,10 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import csv
 from sklearn.cluster import KMeans
-#from datetime import datetime
-#day_of_year = datetime.strptime(date_string, format).timetuple().tm_yday 
-#from enum import Enum
 
-class dataset_attributes:
+class dataset_attributes:               # Για να κάνουμε την ανάθεση λίγο πιο εύκολη
     Travel_Duration=4
-    Traveler_Age=6
+    Traveler_Age=6                      # Τα πεδία που είναι αριθμητικές τιμές είναι μόνο 4. 
     Accomodation_Cost=10
     Transportation_Cost=12
 
@@ -21,10 +18,7 @@ def safe_csv_read(csvstring):            #from python docs: https://docs.python.
             output.append(row)
     return output
 
-#def manhatan_distance(point1_x,point1_y,point2_x,point2_y):   # eucledian distance is harder to compute
-    #return np.abs(point1_x-point2_x) + np.abs(point1_y-point2_y)
-
-def eucledian_distance(point1,point2):
+def eucledian_distance(point1,point2):     # Υπάρχει νομίζω μια συνάρτηση στην numpy αλλά έτσι βολεύομαι περισσότερο.
     return np.sqrt(np.sum(np.square(point1 - point2)))
 
 def split_data(data,cluster_centers,labels):
@@ -32,8 +26,6 @@ def split_data(data,cluster_centers,labels):
     for j in range(0, cluster_centers.shape[0]):
         output.append([])
     k=0
-    #for i in range(0,data.shape[0]):
-        #output[j].append(data[i])
     for i in data:
         output[labels[k]].append(i)
         k=k+1
@@ -44,9 +36,7 @@ def get_cluster_radii(cluster_centers,labels,subset):
     distances=np.zeros((cluster_centers.shape[0],subset.shape[0]))
     spdat=split_data(subset, cluster_centers, labels)
     spdatlen=[]
-    #print("i ran")
-    #print(spdat)
-    #print(spdat[0][0])
+
     for k in range(0,cluster_centers.shape[0]):
         j=0
         for i in spdat[k]:
@@ -59,36 +49,30 @@ def get_cluster_radii(cluster_centers,labels,subset):
         output[i]=np.amax(distances[i])
     return output
 #######################################################################################################
-data=safe_csv_read('source.csv')
-data.pop(0)
-cmap = { 0:'b',1:'y',2:'k',3:'g',4:'r' }
-fig, axis=plt.subplots(2, 3)
+data=safe_csv_read('source.csv')    # Μπορούσα να χρησιμοποιήσω pandas αλλά δεν με βόλευε ο τρόπος που διαβάζει τα csv
+data.pop(0)                         # Αφαιρούμε τους τίτλους απο τα δεδομένα
+cmap = { 0:'b',1:'y',2:'k',3:'g',4:'r' }    # Ένα color map 
+fig, axis=plt.subplots(2, 3)        # Φτιάχνει τα subplots για να εμφανιστούν οι απεικονήσεις. Μια θέση περισεύει. Δεν μας ζητήσατε σχήμα. 
 #######################################################################################################  1
 x=[]
 y=[]
 for i in data:
-    #x.append(pd.to_numeric(i[dataset_attributes().Accomodation_Cost])+pd.to_numeric(i[dataset_attributes().Transportation_Cost]))
-    x.append(pd.to_numeric(i[dataset_attributes().Traveler_Age]))
-    y.append(pd.to_numeric(i[dataset_attributes().Travel_Duration]))
-#plt.scatter(x,y)
+    x.append(pd.to_numeric(i[dataset_attributes().Traveler_Age]))   #Εδώ απομονώνουμε τις στίλες με τις οποίες θέλουμε να κάνουμε τις συστάδες μας 
+    y.append(pd.to_numeric(i[dataset_attributes().Travel_Duration]))    # Απλά τις παίρνουμε μια μια και μετά τις βάζουμε στο subset
+
 subset=np.array(list(zip(x,y)))
-#print(subset)
 
-kmeans= KMeans(n_clusters=2, init='random', n_init='auto' , algorithm='lloyd').fit(subset) # https://scikit-learn.org/stable/modules/generated/sklearn.cluster.KMeans.html
-radii=get_cluster_radii(kmeans.cluster_centers_, kmeans.labels_, subset)
-
-#print(kmeans.cluster_centers_)
-#print(kmeans.labels_)
-#print(radii)
-#print(data)
+kmeans= KMeans(n_clusters=2, init='random', n_init='auto').fit(subset) # https://scikit-learn.org/stable/modules/generated/sklearn.cluster.KMeans.html
+radii=get_cluster_radii(kmeans.cluster_centers_, kmeans.labels_, subset)        # Θέλουμε να έχουμε 2 clusters λόγω εκφώνησης, init="random" γιατί θέλουμε να είναι τυχαία τα αρχικά σημεία και δεν μας ενδιαφέρει.
+                                                                                # Το n_init="auto" λέει πως θα γίνουν 10 επαναλήψεις σύμφωνα με τα docs
 
 j=0
-for i in kmeans.cluster_centers_:
+for i in kmeans.cluster_centers_:                                   # Είναι για τον κύκλο για να εμφανιστεί στο plot
     circle=plt.Circle(i, radii[j], color=cmap[j], fill=False)
     axis[0][0].add_patch(circle)
     j=j+1
 
-axis[0][0].scatter(x,y, c=kmeans.labels_)
+axis[0][0].scatter(x,y, c=kmeans.labels_)   # Ανάθεση σε scatter subplot
 print()
 print("Traveler Age/Travel Duration")
 print("The centroids of the 2 clusters are:")
@@ -96,9 +80,9 @@ print("{} {}".format(kmeans.cluster_centers_[0], kmeans.cluster_centers_[1]))
 print("The radii are:")
 print("{} {}".format(radii[0], radii[1]))
 subsets=split_data(data, kmeans.cluster_centers_, kmeans.labels_)
-#print(subsets[0])
+
 total=0
-for i in data:
+for i in data:              # Τα data, subsets και subsetss (παρακάτω) είναι λίστες και δεν υπάρχει έτοιμη συνάρτηση για να δούμε το μέγεθός τους 
     total=total+1
 cluster1=0
 for i in subsets[0]:
@@ -111,26 +95,16 @@ print()
 print("----------------------------------------------------------------------------------------")
 #######################################################################################################  2
 
-
 x=[]
 y=[]
-for i in subsets[0]:
-    #x.append(pd.to_numeric(i[dataset_attributes().Accomodation_Cost])+pd.to_numeric(i[dataset_attributes().Transportation_Cost]))
+for i in subsets[0]:        #Στην ουσία το ίδιο με το πρώτο αλλά με τα στοιχεία της πρώτης συστάδας
     x.append(pd.to_numeric(i[dataset_attributes().Accomodation_Cost]))
-    #print(i[dataset_attributes().Accomodation_Cost])
     y.append(pd.to_numeric(i[dataset_attributes().Travel_Duration]))
-#plt.scatter(x,y)
+
 subset=np.array(list(zip(x,y)))
 
-#print(subset)
-
-kmeans= KMeans(n_clusters=2, init='random', n_init='auto' , algorithm='lloyd').fit(subset) # https://scikit-learn.org/stable/modules/generated/sklearn.cluster.KMeans.html
+kmeans= KMeans(n_clusters=2, init='random', n_init='auto' ).fit(subset)
 radii=get_cluster_radii(kmeans.cluster_centers_, kmeans.labels_, subset)
-
-#print(kmeans.cluster_centers_)
-#print(kmeans.labels_)
-#print(radii)
-#print(data)
 
 j=0
 for i in kmeans.cluster_centers_:
@@ -149,7 +123,7 @@ print("{} {}".format(kmeans.cluster_centers_[0], kmeans.cluster_centers_[1]))
 print("The radii are:")
 print("{} {}".format(radii[0], radii[1]))
 subsetss=split_data(subsets[0], kmeans.cluster_centers_, kmeans.labels_)
-#print(subsets[0])
+
 total=0
 for i in subsets[0]:
     total=total+1
@@ -163,20 +137,15 @@ print("And the clusters have {} and {} data points out of the {} total points re
 print()
 print("----------------------------------------------------------------------------------------")
 #######################################################################################################  3
+
 x=[]
 y=[]
-for i in subsets[0]:
+for i in subsets[0]:    #Στην ουσία το ίδιο με το πρώτο αλλά με τα στοιχεία της πρώτης συστάδας
     x.append(pd.to_numeric(i[dataset_attributes().Transportation_Cost]))
     y.append(pd.to_numeric(i[dataset_attributes().Travel_Duration]))
 subset=np.array(list(zip(x,y)))
 
-kmeans= KMeans(n_clusters=2, init='random', n_init='auto' , algorithm='lloyd').fit(subset) # https://scikit-learn.org/stable/modules/generated/sklearn.cluster.KMeans.html
-radii=get_cluster_radii(kmeans.cluster_centers_, kmeans.labels_, subset)
-
-#print(kmeans.cluster_centers_)
-#print(kmeans.labels_)
-#print(radii)
-#print(data)
+kmeans= KMeans(n_clusters=2, init='random', n_init='auto' ).fit(subset) 
 
 j=0
 for i in kmeans.cluster_centers_:
@@ -195,7 +164,7 @@ print("{} {}".format(kmeans.cluster_centers_[0], kmeans.cluster_centers_[1]))
 print("The radii are:")
 print("{} {}".format(radii[0], radii[1]))
 subsetss=split_data(subsets[0], kmeans.cluster_centers_, kmeans.labels_)
-#print(subsets[0])
+
 total=0
 for i in subsets[0]:
     total=total+1
@@ -211,21 +180,14 @@ print("-------------------------------------------------------------------------
 #######################################################################################################  4
 x=[]
 y=[]
-for i in subsets[1]:
-    #x.append(pd.to_numeric(i[dataset_attributes().Accomodation_Cost])+pd.to_numeric(i[dataset_attributes().Transportation_Cost]))
+for i in subsets[1]:    #Στην ουσία το ίδιο με το πρώτο αλλά με τα στοιχεία της δεύτερης συστάδας
     y.append(pd.to_numeric(i[dataset_attributes().Traveler_Age]))
     x.append(pd.to_numeric(i[dataset_attributes().Accomodation_Cost]))
-#plt.scatter(x,y)
+
 subset=np.array(list(zip(x,y)))
-#print(subset)
 
-kmeans= KMeans(n_clusters=2, init='random', n_init='auto' , algorithm='lloyd').fit(subset) # https://scikit-learn.org/stable/modules/generated/sklearn.cluster.KMeans.html
+kmeans= KMeans(n_clusters=2, init='random', n_init='auto' ).fit(subset) 
 radii=get_cluster_radii(kmeans.cluster_centers_, kmeans.labels_, subset)
-
-#print(kmeans.cluster_centers_)
-#print(kmeans.labels_)
-#print(radii)
-#print(data)
 
 j=0
 for i in kmeans.cluster_centers_:
@@ -244,7 +206,7 @@ print("{} {}".format(kmeans.cluster_centers_[0], kmeans.cluster_centers_[1]))
 print("The radii are:")
 print("{} {}".format(radii[0], radii[1]))
 subsetss=split_data(subsets[1], kmeans.cluster_centers_, kmeans.labels_)
-#print(subsets[0])
+
 total=0
 for i in subsets[1]:
     total=total+1
@@ -260,21 +222,14 @@ print("-------------------------------------------------------------------------
 #######################################################################################################  5
 x=[]
 y=[]
-for i in subsets[1]:
-    #x.append(pd.to_numeric(i[dataset_attributes().Accomodation_Cost])+pd.to_numeric(i[dataset_attributes().Transportation_Cost]))
+for i in subsets[1]:    #Στην ουσία το ίδιο με το πρώτο αλλά με τα στοιχεία της δεύτερης συστάδας
     y.append(pd.to_numeric(i[dataset_attributes().Traveler_Age]))
     x.append(pd.to_numeric(i[dataset_attributes().Transportation_Cost]))
-#plt.scatter(x,y)
+
 subset=np.array(list(zip(x,y)))
-#print(subset)
 
-kmeans= KMeans(n_clusters=2, init='random', n_init='auto' , algorithm='lloyd').fit(subset) # https://scikit-learn.org/stable/modules/generated/sklearn.cluster.KMeans.html
+kmeans= KMeans(n_clusters=2, init='random', n_init='auto' ).fit(subset) 
 radii=get_cluster_radii(kmeans.cluster_centers_, kmeans.labels_, subset)
-
-#print(kmeans.cluster_centers_)
-#print(kmeans.labels_)
-#print(radii)
-#print(data)
 
 j=0
 for i in kmeans.cluster_centers_:
@@ -293,7 +248,7 @@ print("{} {}".format(kmeans.cluster_centers_[0], kmeans.cluster_centers_[1]))
 print("The radii are:")
 print("{} {}".format(radii[0], radii[1]))
 subsetss=split_data(subsets[1], kmeans.cluster_centers_, kmeans.labels_)
-#print(subsets[0])
+
 total=0
 for i in subsets[1]:
     total=total+1
