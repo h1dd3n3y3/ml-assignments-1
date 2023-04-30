fab1c3b5b16d8e5ba7ca11578bccad6dddf71115
david011011 <parathyrasdavid@gmail.com>
2023-04-30 12:33:00 +0300

    Attatch run results

--- ERG1/erg1.py
+++ ERG1/erg1.py
@@ -1,3 +1,40 @@
+# Run result for given dates:
+"""
+Dataset dates: 2020-01-01 - 2021-01-01
+Enter a prediction starting date > '2021-01-01' (in YYYY-MM-DD format): 2021-03-01
+Enter a prediction ending date > '2021-03-01' (in YYYY-MM-DD format): 2022-03-01
+
+
+Filtered 12/281 total 'AAPL' stock data between 2020-01-01 - 2021-01-01.
+
+[{'timestamp': '2020-01-31', 'open': '296.2400', 'high': '327.8500', 'low': '292.7500', 'close': '309.5100', 'adjusted close': '75.6888', 'volume': '734044103', 'dividend amount': '0.0000'}, {'timestamp': '2020-02-28', 'open': 
+'304.3000', 'high': '327.2200', 'low': '256.3700', 'close': '273.3600', 'adjusted close': '67.0094', 'volume': '755223231', 'dividend amount': '0.7700'}, {'timestamp': '2020-03-31', 'open': '282.2800', 'high': '304.0000', 'low': '212.6100', 'close': '254.2900', 'adjusted close': '62.3347', 'volume': '1570331732', 'dividend amount': '0.0000'}, {'timestamp': '2020-04-30', 'open': '246.5000', 'high': '294.5300', 'low': '236.9000', 'close': '293.8000', 'adjusted close': '72.0199', 'volume': '816530808', 'dividend amount': '0.0000'}, {'timestamp': '2020-05-29', 'open': '286.2500', 'high': '324.2400', 'low': '285.8500', 'close': '317.9400', 'adjusted close': '78.1435', 'volume': '701660022', 'dividend amount': '0.8200'}, {'timestamp': '2020-06-30', 'open': '317.7500', 'high': '372.3800', 'low': '317.2100', 'close': '364.8000', 'adjusted close': '89.6608', 'volume': '810900890', 'dividend amount': '0.0000'}, {'timestamp': '2020-07-31', 'open': '365.1200', 'high': '425.6600', 'low': '356.5800', 'close': '425.0400', 'adjusted close': '104.4666', 'volume': '755162226', 'dividend amount': '0.0000'}, {'timestamp': '2020-08-31', 'open': '432.8000', 'high': '515.1400', 'low': '126.0000', 'close': '129.0400', 'adjusted close': '127.0962', 'volume': '1184207050', 'dividend amount': '0.8200'}, {'timestamp': '2020-09-30', 'open': '132.7600', 'high': '137.9800', 'low': '103.1000', 'close': '115.8100', 'adjusted close': '114.0655', 'volume': '3886793083', 'dividend amount': '0.0000'}, {'timestamp': '2020-10-30', 'open': '117.6400', 'high': '125.3900', 'low': '107.7200', 'close': '108.8600', 'adjusted close': '107.2201', 'volume': '2895317580', 'dividend amount': '0.0000'}, {'timestamp': '2020-11-30', 'open': '109.1100', 'high': '121.9900', 'low': '107.3200', 'close': '119.0500', 'adjusted close': '117.4592', 'volume': '2122724412', 'dividend amount': '0.2050'}, {'timestamp': '2020-12-31', 'open': '121.0100', 'high': '138.7890', 'low': '120.0100', 'close': '132.6900', 'adjusted close': '130.9169', 'volume': '2319687808', 'dividend amount': '0.0000'}]
+
+
+Filtered 12/281 total 'AAPL' stock data between 2021-03-01 - 2022-03-01:
+
+[{'timestamp': '2021-03-31', 'open': '123.7500', 'high': '128.7200', 'low': '116.2100', 'close': '122.1500', 'adjusted close': '120.6984', 'volume': '2650845211', 'dividend amount': '0.0000'}, {'timestamp': '2021-04-30', 'open': '123.6600', 'high': '137.0700', 'low': '122.4900', 'close': '131.4600', 'adjusted close': '129.8978', 'volume': '1889956694', 'dividend amount': '0.0000'}, {'timestamp': '2021-05-28', 'open': '132.0400', 'high': '134.0700', 'low': '122.2500', 'close': '124.6100', 'adjusted close': '123.3372', 'volume': '1711935110', 'dividend amount': '0.2200'}, {'timestamp': '2021-06-30', 'open': '125.0800', 'high': '137.4100', 'low': '123.1300', 'close': '136.9600', 'adjusted close': '135.5611', 'volume': '1606114354', 'dividend amount': '0.0000'}, {'timestamp': '2021-07-30', 'open': '136.6000', 'high': '150.0000', 'low': '135.7600', 'close': '145.8600', 'adjusted close': '144.3701', 'volume': '1916751489', 'dividend amount': '0.0000'}, {'timestamp': '2021-08-31', 'open': '146.3600', 'high': '153.4900', 'low': '144.5000', 'close': '151.8300', 'adjusted close': '150.5054', 'volume': '1462773381', 'dividend amount': '0.2200'}, {'timestamp': '2021-09-30', 'open': '152.8300', 'high': '157.2600', 'low': '141.2700', 'close': '141.5000', 'adjusted close': '140.2655', 'volume': '1797948421', 'dividend amount': '0.0000'}, {'timestamp': '2021-10-29', 'open': '141.9000', 'high': '153.1650', 'low': '138.2700', 'close': '149.8000', 'adjusted close': '148.4931', 'volume': '1565079040', 'dividend amount': '0.0000'}, {'timestamp': '2021-11-30', 'open': '148.9850', 'high': '165.7000', 'low': '147.4800', 'close': '165.3000', 'adjusted close': '164.0962', 'volume': '1688864233', 'dividend amount': '0.2200'}, {'timestamp': '2021-12-31', 'open': '167.4800', 'high': '182.1300', 'low': '157.8000', 
+'close': '177.5700', 'adjusted close': '176.2768', 'volume': '2443227128', 'dividend amount': '0.0000'}, {'timestamp': '2022-01-31', 'open': '177.8300', 'high': '182.9400', 'low': '154.7000', 'close': '174.7800', 'adjusted close': '173.5071', 'volume': '2106513962', 'dividend amount': '0.0000'}, {'timestamp': '2022-02-28', 'open': '174.0100', 'high': '176.6500', 'low': '152.0000', 'close': '165.1200', 'adjusted close': '164.1267', 'volume': '1628570227', 'dividend amount': '0.2200'}]
+
+
+Real time stock values: [186.82, 198.31, 195.19, 198.97, 217.88, 225.74, 227.89, 222.3, 239.44, 261.03, 260.29, 252.65]
+Predicted stock values: [568.71, 541.17, 513.63, 486.09, 458.55, 431.01, 403.47, 375.93, 348.39, 320.85, 293.31, 265.77]
+
+
+Prediction error for 2021-03-31: |186.82 - 568.71| = 381.89
+Prediction error for 2021-04-30: |198.31 - 541.17| = 342.86
+Prediction error for 2021-05-28: |195.19 - 513.63| = 318.44
+Prediction error for 2021-06-30: |198.97 - 486.09| = 287.12
+Prediction error for 2021-07-30: |217.88 - 458.55| = 240.67
+Prediction error for 2021-08-31: |225.74 - 431.01| = 205.27
+Prediction error for 2021-09-30: |227.89 - 403.47| = 175.58
+Prediction error for 2021-10-29: |222.3 - 375.93| = 153.63
+Prediction error for 2021-11-30: |239.44 - 348.39| = 108.95
+Prediction error for 2021-12-31: |261.03 - 320.85| = 59.82
+Prediction error for 2022-01-31: |260.29 - 293.31| = 33.02
+Prediction error for 2022-02-28: |252.65 - 265.77| = 13.12
+Continue predictions for new dates? (yes/no): """
+
 import requests, csv, numpy as np, matplotlib.pyplot as plt, os, platform
 from datetime import datetime
 
